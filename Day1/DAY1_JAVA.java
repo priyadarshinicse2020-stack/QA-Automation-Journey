44b9930e63efee61395fa8d062e56054834df462
Priya <priyadarshinicse2020@gmail.com>
2026-09-06 22:14:43 +0530

    Day 1 - Java basics practice

---
+++ Day1/DAY1_JAVA.java
@@ -0,0 +1,46 @@
+
+public class DAY1_JAVA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int num1 = 27;
+		int num2 = 78;
+		int num3 = 87; 
+		int num4 = 64;
+		//9. WAP to define the interest rate on the basis of Loan type using 
+		//Switch Case Loan Type: Car Loan, Housing Loan, Personal Loan, Education Loan
+		//For Housing Loan, if user’s salary is less than 35000 USD - print : NOT APPLICABLE FOR Housing Loan
+		
+		if((num1) >= (num2))
+			
+		{
+			System.out.println("num 1 is greatest");
+			
+		}
+		else if((num2) >= (num3))
+		{
+			System.out.println("num 2 is greteast");
+			
+		}
+		else if((num2) >= (num4))
+       {
+	 System.out.println("num 2 is gretaest ");
+       }
+		else if ((num3) >= (num4)) {
+			System.out.println("num 2 is gretaest ");
+		}
+		else 
+		{
+			System.out.println("num4 is gretaest ");
+			
+		}
+		
+		
+		
+		
+	}
+
+}
